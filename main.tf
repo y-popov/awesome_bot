2e7972cc5b78cb00582d5eaa2b4467103ec5b6ea
megamad <ya.popoff@gmail.com>
2021-08-08 15:54:22 +0300

    Поправил форматирование

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     yandex = {
-      source   = "yandex-cloud/yandex"
+      source = "yandex-cloud/yandex"
     }
   }
   backend "s3" {
-    endpoint   = "storage.yandexcloud.net"
-    bucket     = "mad-bucket"
-    key        = "awesome-bot.tfstate"
-    region     = "ru-central1-a"
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "mad-bucket"
+    key                         = "awesome-bot.tfstate"
+    region                      = "ru-central1-a"
     skip_region_validation      = true
     skip_credentials_validation = true
   }
 }
 
 provider "yandex" {
-  zone         = "ru-central1-a"
+  zone = "ru-central1-a"
 }
 
 data "yandex_function_trigger" "timer" {
